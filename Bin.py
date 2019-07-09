13e91fc1e790b5819fe3131b751429046eff4959
Augusto Carmona Valencia <acarmon2@gmail.com>
2019-07-09 00:46:58 -0500

    Add files via upload

---
+++ Bin.py
@@ -0,0 +1,31 @@
+# Function to calculate the Bin generators based in article 1997
+# The program produce a random search in the permutations to get
+# the D vector with F properties
+
+import numpy as np
+import math
+from random import randrange
+
+def Bin(q, d, NT):
+    #################################################################################################
+    # Function to produce the random sequence with the first condition
+    # of Bin construction.
+    def RandomS(q, d):
+        L = q - (2*d) - 1
+        V0 = np.zeros(L)
+        Seq0 = np.zeros(L)
+        for i in range(0,(L/2)):
+            V0[i] = d + i + 1
+            V0[(L/2)+i]= q - V0[i]
+        # Random permutation of the original sequence to check the second 
+        # condition.
+        T0 = L
+        for i in range (0,(L/2)):
+            cont = randrange(T0)
+            Seq0[i] = V0[cont]
+            Seq0[i+(L/2)] = (q - V0[cont])
+            V0 = V0[V0 != Seq0[i]]
+            V0 = V0[V0 != (q - Seq0[i])]
+            T0 = T0 - 2
+        return Seq0
+
