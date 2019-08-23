25b93585825ec3dddaecabc16dfd60f7939b5e37
Augusto Carmona Valencia <acarmon2@gmail.com>
2019-08-22 20:06:20 -0500

    Add Bin conditions
    Search a different amount of Bin codes

--- Bin.py
+++ Bin.py
@@ -57,7 +57,6 @@ def Bin(q, d, NT):
             flag = False
         return flag
 
-
     #################################################################################################
     # Function to produce the User codes using the Generator vector Gen0. The final matrix 
     def UCodes(Gen0, q):
@@ -75,26 +74,62 @@ def Bin(q, d, NT):
             UCodes[i][:] = np.mod(((UCodes[i-1][:])+1),q)
         return UCodes
 
+    #################################################################################################
+    # Function to check the cross correlation Hamming function, it returns the maximum value of cross
+    # correlation function
+    def HammingCC(C0, C1):
+        CCVector0 = np.()
+        np.roll()
+
+    #################################################################################################
+    # Function to check the matrix of codes achived the Bin conditions of one coincident and minimum 
+    # spectral distance between adjacent symbols
+    def CheckBin(U0,d):
+        
+    
     ###################################################################################################
     # Main function to calculate NT chances of random permutation to find differents generator vectors,
-    # To save the "possible" N options, the maximum case is 100 in this case
+    # To save the "possible" N options, the maximum case is 100 in this case, the system save the number
+    # of generators and the user codes respectively to generator. Bcont is the cont to check the number
+    # of codes founded it
     L0 = q - (2*d) - 1
+    Bcont = 0
     BCodes = np.zeros((100,q,L0), dtype = np.int_)
+    BGenerators = np.zeros((100,L0), dtype = np.int_)
     # Search the first family of codes
     for i in range(0,NT):
         G0 = RandomS(q,d)
         G1 = Dtest(G0,q)
         if(G1):
-            BCodes[0][:][:]=UCodes(G0,q)
+            BCodes[0][:][:] = UCodes(G0,q)
+            BGenerators[0][:] = G0
             Bcont = 1
             break
-        # Generator no found
-        if((not G1) and (i == NT)):
-            print "Generator not found"
-    # Add not repetitive generator family
-    for j in range(0,NT):
+        # Generator no founded it
+    if(Bcont == 0):
+        print "First trial! Generator not founded it"
+    # Add not repetitive generator family, second change of find another famillies of codes if the first code 
+    # it is not founded it
+    for i in range(0,NT):
         G0 = RandomS(q,d)
         G1 = Dtest(G0,q)
+        # Check of the generator is not repetitive with previous generators founded it yet
+        cont = 0
         if(G1):
-            for k in range(0,Bcont):
-                A0 = 
+            for k in range(0, Bcont):
+                if(np.array_equal(G0,BGenerators[k][:])):
+                    break
+                else:
+                    cont = cont + 1
+            if(cont == Bcont):
+                BGenerators[Bcont][:] = G0
+                BCodes[Bcont][:][:] = UCodes(G0,q)
+                Bcont = Bcont + 1
+    # Return the complete tensor with the codes or generator no found
+    if(Bcont == 0):
+        print "Second trial! Generator not founded it"
+        Codes = False
+    else:
+        Codes = BCodes[0:Bcont][:][:]
+
+return Codes
